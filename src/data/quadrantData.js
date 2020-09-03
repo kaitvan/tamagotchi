45efb47f6928d3feccf65043bb1a72272a73fa88
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-09-03 18:35:38 -0500

    built array of objects with quadrant data

---
+++ src/data/quadrantData.js
@@ -0,0 +1,22 @@
+const quadrantData = [
+  {
+    name: 'eat',
+    fullnessScore: 100,
+  },
+  {
+    name: 'play',
+    funScore: 50,
+  },
+  {
+    name: 'fight',
+    strengthScore: 100,
+  },
+  {
+    name: 'sleep',
+    energyScore: 50,
+  }
+];
+
+const getQuadrantData = () => quadrantData;
+
+export default { getQuadrantData };

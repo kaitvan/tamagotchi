9c688efe09b873137f81594e824d89e9dca4d3cd
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-09-05 17:05:28 -0400

    eat buttons working

--- src/javascripts/components/displayQuadrants.js
+++ src/javascripts/components/displayQuadrants.js
@@ -1,17 +1,7 @@
-import Quadrants from './makeQuadrants';
+import Eat from './eatQuadrant';
 
-const displayQuadrants = (arr) => {
-  arr.forEach((obj) => {
-    if (obj.name === 'Eat') {
-      $('#eat').append(Quadrants.makeQuadrants(obj));
-    } else if (obj.name === 'Play') {
-      $('#play').append(Quadrants.makeQuadrants(obj));
-    } else if (obj.name === 'Fight') {
-      $('#fight').append(Quadrants.makeQuadrants(obj));
-    } else if (obj.name === 'Sleep') {
-      $('#sleep').append(Quadrants.makeQuadrants(obj));
-    }
-  });
+const displayQuadrants = () => {
+  Eat.displayEatQuadrant();
 };
 
 export default { displayQuadrants };

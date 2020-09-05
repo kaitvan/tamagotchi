47e53296801b440555f6e0d5c6c365f58d14bd32
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-09-05 12:45:59 -0400

    name, buttons, and level print to the dom in correct containers

--- src/javascripts/components/displayQuadrants.js
+++ src/javascripts/components/displayQuadrants.js
@@ -2,7 +2,15 @@ import makeQuadrants from './makeQuadrants';
 
 const displayQuadrants = (arr) => {
   arr.forEach((obj) => {
-    $('#app').append(makeQuadrants.makeQuadrants(obj));
+    if (obj.name === 'Eat') {
+      $('#eat').append(makeQuadrants.makeQuadrants(obj));
+    } else if (obj.name === 'Play') {
+      $('#play').append(makeQuadrants.makeQuadrants(obj));
+    } else if (obj.name === 'Fight') {
+      $('#fight').append(makeQuadrants.makeQuadrants(obj));
+    } else if (obj.name === 'Sleep') {
+      $('#sleep').append(makeQuadrants.makeQuadrants(obj));
+    }
   });
 };
 
